d782cb71c477bef8784d3066b3444ed1b20580c7
Doron Segal <doron2402@gmail.com>
2015-09-15 16:54:04 -0700

    test are done

--- src/plugin.js
+++ src/plugin.js
@@ -2,14 +2,15 @@
 
 exports.register = function(plugin, options, next){
   //Check if header contains version
+  var DEFAULT_HEADER_KEY = 'version';
   var DEFAULT_PATTERN = /^(v[1-9])$/;
+  var versionHeader = options.header || DEFAULT_HEADER_KEY;
   plugin.ext('onRequest', function(request, reply) {
     var urlPath = request.url.pathname.split('/');
     urlPath[0] === '' ? urlPath.shift() : null;
     var pattern = options.pattern || DEFAULT_PATTERN;
-    if (pattern.test(request.headers.version) && !pattern.test(urlPath[0])){
-      urlPath.unshift('',request.headers.version);
-
+    if (pattern.test(request.headers[versionHeader]) && !pattern.test(urlPath[0])){
+      urlPath.unshift('',request.headers[versionHeader]);
       request.setUrl(urlPath.join('/'));
     }
     reply.continue();
